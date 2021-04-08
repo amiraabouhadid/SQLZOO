a8bed2aee5ca4cb7c3bc1b94dc471492e0eedfb4
amira <amiraabouhadid@gmail.com>
2021-04-08 20:08:39 +0200

    add solutions to SELECT basics, world, nobel, SELECT and SUM & COUNT

--- answers.sql
+++ answers.sql
@@ -89,4 +89,45 @@ SELECT winner, yr, subject FROM nobel WHERE
 winner LIKE 'Sir%' ORDER BY yr DESC ;
 SELECT winner, subject FROM nobel WHERE yr = 1984
 ORDER BY
-CASE WHEN subject IN ('Physics', 'Chemistry') THEN 1 ELSE 0 END, subject, winner; 
+CASE WHEN subject IN ('Physics', 'Chemistry') THEN 1 ELSE 0 END, subject, winner;
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
+SELECT name FROM world WHERE gdp/population >  (SELECT gdp/population FROM world WHERE name = 'United Kingdom') AND continent = 'Europe';
+SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name IN ('Argentina', 'Australia')) ORDER BY name;
+SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland');
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany')*100 ) , '%') AS percentage FROM world WHERE continent = 'Europe';
+SELECT name FROM world WHERE gdp > ALL(SELECT gdp FROM world  WHERE continent = 'Europe' AND gdp >0)
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+SELECT continent, name FROM world x WHERE name <= ALL(SELECT name FROM world y WHERE x.continent = y.continent);
+SELECT name, continent, population FROM world WHERE continent IN (SELECT continent FROM world  x WHERE 25000000 >= (SELECT MAX(population) FROM world y WHERE x.continent = y.continent));
+SELECT name, continent FROM world x
+  WHERE population > ALL(SELECT 3*population FROM world y WHERE x.continent = y.continent AND x.name <> y.name)
+SELECT region, name, population FROM bbc x WHERE population <= ALL (SELECT population FROM bbc y WHERE y.region=x.region AND population>0)
+SELECT name,region,population FROM bbc x WHERE 50000 < ALL (SELECT population FROM bbc y WHERE x.region=y.region AND y.population>0)
+SELECT name, region FROM bbc x
+ WHERE population < ALL (SELECT population/3 FROM bbc y WHERE y.region = x.region AND y.name != x.name)
+SELECT name FROM bbc
+WHERE gdp > (SELECT MAX(gdp) FROM bbc WHERE region = 'Africa')
+SELECT name FROM bbc
+ WHERE population < (SELECT population FROM bbc WHERE name='Russia')
+   AND population > (SELECT population FROM bbc WHERE name='Denmark')
+SELECT SUM(population)
+   FROM world
+SELECT DISTINCT continent FROM world
+SELECT SUM(gdp) FROM world WHERE continent = 'Africa'
+SELECT COUNT(name) FROM world WHERE area >= 1000000
+SELECT SUM(population) FROM world WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
+SELECT continent, count(name) FROM world GROUP BY continent
+SELECT continent, COUNT (name) FROM world WHERE population >= 10000000 GROUP BY continent
+SELECT continent FROM world x WHERE (SELECT SUM(population) FROM world y WHERE  x.continent = y.continent) >= 100000000 GROUP BY continent
+SELECT name, population/area AS density FROM bbc WHERE population = (SELECT MAX(population) FROM bbc)
+SELECT region, SUM(population)/SUM(area) AS density FROM bbc GROUP BY region
+SELECT AVG(population) FROM bbc WHERE name IN ('Poland', 'Germany', 'Denmark')
+SELECT COUNT(name) FROM bbc WHERE population < 150000
+SELECT SUM(population) FROM bbc WHERE region = 'Europe'
