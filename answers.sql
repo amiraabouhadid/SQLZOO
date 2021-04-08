c5baeb2d3ed72e71a3d575a1baeea9d1680a3141
amira <amiraabouhadid@gmail.com>
2021-04-09 00:39:53 +0200

    Add Self JOIN

--- answers.sql
+++ answers.sql
@@ -254,4 +254,44 @@ END
 SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
 SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
 SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
- 
+SELECT COUNT(*) FROM stops
+SELECT id FROM stops WHERE name = 'Craiglockhart'
+SELECT id, name FROM stops JOIN route ON stops.id = stop WHERE num = '4' AND company = 'LRT'
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) =2
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop = 149
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=115 AND b.stop = 137
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'Tollcross'
+SELECT DISTINCT stopb.name, a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'
+SELECT a.num, a.company,  stops.name,  d.num, d.company
+FROM route a JOIN route b ON a.company = b.company AND a.num = b.num
+JOIN stops ON b.stop = stops.id
+JOIN route c ON c.stop = stops.id
+JOIN route d ON c.company = d.company AND c.num = d.num
+WHERE a.stop = (SELECT id FROM stops WHERE name = 'Craiglockhart')
+AND d.stop = (SELECT id FROM stops WHERE name = 'Lochend')
+ORDER BY a.num, stops.name, d.num
